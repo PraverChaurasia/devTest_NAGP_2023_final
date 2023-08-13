afe71555878d25b3d926774801c8d43423184a7e
PraverChaurasia <101858516+PraverChaurasia@users.noreply.github.com>
2023-08-13 16:45:21 +0530

    Update SearchTest.java

--- src/test/java/test/SearchTest.java
+++ src/test/java/test/SearchTest.java
@@ -11,12 +11,7 @@ public class SearchTest extends BaseTest{
 	@Test
 	public static void search(){
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nvidia 3090");
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		driver.findElement(By.id("nav-search-submit-button")).click();
 		try {
 			Thread.sleep(2000);
